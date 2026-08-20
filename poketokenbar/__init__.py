baac3f3b471f6f5942b34af8e35fdb1e13ebc1ec
EunjunChoi <choiej@postech.ac.kr>
2026-08-20 13:20:21 +0900

    Bump version to 1.2.1

--- poketokenbar/__init__.py
+++ poketokenbar/__init__.py
@@ -2,5 +2,5 @@
 PokeTokenBar CLI - Turn your AI coding tokens into a Pokémon companion in your terminal!
 """
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "PokeTokenBar Contributors"
