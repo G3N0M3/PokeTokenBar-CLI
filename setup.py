ac171d4d0181852a7abb039175265bc1235836f0
EunjunChoi <choiej@postech.ac.kr>
2026-08-26 06:15:45 +0000

    chore(release): bump version to 1.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="poketokenbar",
-    version="1.4.2",
+    version="1.5.0",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
